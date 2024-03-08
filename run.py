e8e0e341d476e3fa7972381b6edfa0f0fd6325e8
Tom Nil <tnil1143@gmail.com>
2024-03-08 01:48:21 +0000

    Battleship object and function creating a ships

--- run.py
+++ run.py
@@ -24,3 +24,22 @@ class GameBoard:
       for row in self.board:
           print("%d|%s|" % (row_number, "|".join(row)))
           row_number += 1
+
+class Battleship:
+    """
+    Battleship object
+    """
+    def __init__(self, board):
+        self.board = board
+
+    def create_ships(self):
+        """
+        Function which randonly putting a ships on board coordinates
+        """
+        for i in range(5):
+            self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
+            while self.board[self.x_row][self.y_column] == "X":
+                self.x_row, self.y_column = random.randint(0, 7), random.randint(0, 7)
+            self.board[self.x_row][self.y_column] = "X"
+        return self.board
+        
