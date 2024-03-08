e86fe973fdc462770fdd6118fe7d32eca0422fe3
Tom Nil <tnil1143@gmail.com>
2024-03-08 01:42:17 +0000

    Game board object with board to print

--- run.py
+++ run.py
@@ -1,3 +1,26 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class GameBoard:
+  """
+  Game board object
+  """
+  def __init__(self, board):
+    self.board = board
+
+  def get_letters_to_numbers():
+    """
+    Convert letters to the numbers
+    """
+    letters_to_numbers = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
+    return letters_to_numbers
+
+  def print_board(self):
+      """
+      Print the board
+      """
+      print("  A B C D E F G H")
+      print("  +-+-+-+-+-+-+-+")
+      row_number = 1
+      for row in self.board:
+          print("%d|%s|" % (row_number, "|".join(row)))
+          row_number += 1
