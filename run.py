2057721290a6e4ae737836c7e6058bd1848d7d10
Tom Nil <tnil1143@gmail.com>
2024-03-09 00:38:00 +0000

    fixed bug with empty input

--- run.py
+++ run.py
@@ -62,14 +62,14 @@ class Battleship:
         """
         try:
             x_row = input("Enter the row of the ship (1-8):\n")
-            while x_row not in "12345678":
+            while x_row not in "12345678" or len(x_row) == 0:
                 print("Please select a valid row")
                 x_row = input("Enter the row of the ship (1-8):\n")
 
             y_column = input("Enter the column of the ship (A-H):\n").upper()
-            while y_column not in "ABCDEFGH":
+            while y_column not in "ABCDEFGH" or len(y_column) == 0:
                 print("Please select a valid column")
-                y_column = input("Enter the column of the ship (A-H):\n").upper()
+                y_column = input("Enter a column of the ship (A-H):\n").upper()
             return int(x_row) - 1, GameBoard.get_letters_to_numbers()[y_column]
         except ValueError and KeyError:
             print("Not a valid input")
